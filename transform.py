644d7ac52624e811479856abc8b6bcbeae569be7
Mike Fowler <fowler_mychale@yahoo.com>
2019-05-08 08:29:02 -0700

    Seeks out specific patterns in email addresses.
    This script locates email addresses that follow a specific naming convention.:
       
       Mike.JonesDF@example.com
       MylesgNd@example.com
       RufinoS@example.com
    
    From those examples the script will locate email address whose last two letters are uppercase and place those results into a new csv file for review.

---
+++ transform.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+import sys
+import argparse
+from datetime import datetime
+
+def parse_arg():
+
+    """Parses the args"""    
+    if len(sys.argv) < 1:
+        print("Args: Input filename")
+        raise RuntimeError("Insufficient arguments.")
+    
+    arg_1 = sys.argv[1]
+    #arg_2 = sys.argv[0]
+    return arg_1
+
+#Parameter is the filename
+filename = parse_arg()
+
+#Create filename for output
+output_file_date = datetime.now().strftime("%Y%m%d_%I%M%S")
+output_file = output_file_date + '_' + filename
+
+#Access the input file
+with open(filename, 'rb') as input_file, open(output_file, 'w') as file_output:
+    
+    #Create the header"""
+    output_file_header = input_file.readline().decode().replace('\r','')
+    file_output.write(output_file_header)
+
+    #Skip the header in the input file
+    next(input_file)
+
+    #iterate over file object line by line
+    for line in input_file:
+
+        #Create output file objects
+        lines = line.decode().split(',')
+
+        #Create the email username variable
+        username = lines[1].split('@')[0]
+
+        #Create variable that display last two characters
+        check_email = username[-1:]
+        
+        #Create file based on business rules and write results to file.
+        if check_email.isupper(): #and len(username) < 16:
+            output_data = ','.join(lines).replace('\r','')
+            file_output.write(output_data)
