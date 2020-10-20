94586b9567315f8602c34a71b28f4fa71d924763
Mike Fowler <fowler_mychale@yahoo.com>
2020-10-20 13:19:54 -0700

    Added popup for error handling.

--- CamelCaseCheckApp.py
+++ CamelCaseCheckApp.py
@@ -2,23 +2,24 @@ from pandas import pandas as pd
 from time import strftime
 import PySimpleGUI as sg
 import os
+import traceback
 def pget(newFile):
-    #read from csv
-    df = pd.read_csv(newFile)
-    #drop NULL vakue from Answer column
-    df.dropna(axis=0, subset=['Check'],inplace = True)
-    #function to check CamelCase
-    def is_camel_case(s):
-      if s != s.lower() and s != s.upper() and "_" not in s and sum(i.isupper() for i in s[1:-1]) == 2:
-          return True
-      return False
-    #run is_camel_case function on column 'Row' in dataframe
-    df['Row'] = df['Check'].apply(is_camel_case)
-    #drop row if Boolean False
-    df.drop(df.loc[df['Row'] == False].index, inplace=True)
-    #Save to new csv file
-    newFileName = os.path.splitext(newFile)[0]
-    pd.DataFrame.to_csv(df,newFileName + '_' + strftime('%Y-%m-%d') + ".csv",',') 
+		#read from csv
+		df = pd.read_csv(newFile)
+		#drop NULL vakue from Answer column
+		df.dropna(axis=0, subset=['Answer'],inplace = True)
+		#function to check CamelCase
+		def is_camel_case(s):
+			if s != s.lower() and s != s.upper() and "_" not in s and sum(i.isupper() for i in s[1:-1]) == 2:
+					return True
+			return False
+		#run is_camel_case function on column 'Row' in dataframe
+		df['Row'] = df['Answer'].apply(is_camel_case)
+		#drop row if Boolean False
+		df.drop(df.loc[df['Row'] == False].index, inplace=True)
+		#Save to new csv file
+		newFileName = os.path.splitext(newFile)[0]
+		pd.DataFrame.to_csv(df,newFileName + '_' + strftime('%Y-%m-%d') + ".csv",',')
 #gui setup
 sg.theme('Reddit')
 layout = [[sg.Input(), sg.FileBrowse(file_types=(("Csv Files", "*.csv"),("Text Files", "*.txt")))],
@@ -26,16 +27,17 @@ layout = [[sg.Input(), sg.FileBrowse(file_types=(("Csv Files", "*.csv"),("Text F
          [sg.Button('Run'), sg.Exit()]]
 window = sg.Window('Pattern Application',layout)
 while True:  # Event Loop
-    event, values = window.read()
-    if event == sg.WIN_CLOSED or event  == 'Exit':
-      break
-    if event == 'Run':
-        try:
-        	try:
-        		pget(values[0])
-        		sg.popup('Job Completed.')
-        	except KeyError as err:
-        		sg.Print(err, 'Error with the input file. Please check it and try again.')
-        except Exception as err:
-        	sg.Print(err)
+		event, values = window.read()
+		if event == sg.WIN_CLOSED or event  == 'Exit':
+			break
+		if event == 'Run':
+				try:
+					try:
+						pget(values[0])
+						sg.popup('Job Completed. File can be found at:\n' + values[0])
+					except KeyError as err:
+								#sg.Print(traceback.format_exc())
+								sg.popup_error(traceback.format_exc())
+				except Exception as err:
+					sg.Print(err)
 window.close()
